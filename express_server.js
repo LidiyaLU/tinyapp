81416e11509a46a2e39769c7cae274979256b5ed
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-10 00:31:49 +0000

    implement redirect after form submission and redirect short URL

--- express_server.js
+++ express_server.js
@@ -41,8 +41,16 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandonString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/" + shortURL);
+
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(`we have ${longURL} and  ${req.params.shortURL}`)
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
