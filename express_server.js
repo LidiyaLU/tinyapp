09bf61d2b5c9ab25c149de1002d4f39936cabad1
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-11 01:05:57 +0000

    implement logout, create logout path and clear cookies after logout

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req,res) => {
-  const templateVars = {urls:urlDatabase};
+  const templateVars = {urls:urlDatabase, username: req.cookies["username"],};
   res.render('urls_index', templateVars)
 })
 
@@ -38,7 +38,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],  username: req.cookies["username"]}; 
   res.render("urls_show", templateVars);
 });
 
@@ -68,7 +68,11 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
-  console.log(req.body.username);
+  res.redirect("/urls");
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
